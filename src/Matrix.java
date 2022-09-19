979fe7d68263191d63b9c74b8b14f493651a783c
vierifirdaus3 <13521099@std.stei.itb.ac.id>
2022-09-19 20:18:56 +0700

    nambahin class matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,9 @@
+class matrix {
+    public double [][] array= new double[100][100] ;
+    public int row;
+    public int col;
+}
+
 public class Matrix {
     static int Jumlah(int a, int b){
         return a+b;
