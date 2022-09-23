86e5df3182e46819375e22ae357009479c4a1538
vierifirdaus <16521294@mahasiswa.itb.ac.id>
2022-09-24 01:53:30 +0700

    SPL Gauss, SPL GaussJordan, Regresi linier berganda udah

--- src/Matrix.java
+++ src/Matrix.java
@@ -223,7 +223,25 @@ class matriks {
                 for(int i=idx;i<this.row;i++){
                     pengurang=this.array[i][acuan]/this.array[idx-1][acuan];
                     if(pengurang!=0){
-                        System.out.printf("R%d -> R%d + %.2f*R%d\n",i+1,i+1,pengurang,idx);
+                        if(pengurang>0.0){
+                            if(this.array[i][acuan]==this.array[idx-1][acuan]){
+    
+                                System.out.printf("R%d -> R%d - R%d\n",i+1,i+1,idx);
+                            }
+                            else{
+                                System.out.printf("R%d -> R%d - %.2f*R%d\n",i+1,i+1,pengurang,idx);
+                            }
+                        }
+                        else{
+                            if(this.array[idx-1][acuan]==this.array[i][acuan]){
+    
+                                System.out.printf("R%d -> R%d + R%d\n",i+1,i+1,idx);
+                            }
+                            else{
+                                System.out.printf("R%d -> R%d + %.2f*R%d\n",i+1,i+1,-pengurang,idx);
+                            }
+                        }
+                        // System.out.printf("R%d -> R%d + %.2f*R%d\n",i+1,i+1,pengurang,idx);
                         Display();
                         System.out.println();
                         for(int j=acuan;j<this.col;j++){
@@ -685,6 +703,188 @@ class matriks {
         
 
     }
+
+    void Hasil_OBE(){
+        int idx;
+        double pengurang;
+        idx=1;
+        int [] nol = new int[10];
+        while(!Eselon() || idx<this.row){
+            for(int i=0;i<this.row;i++){
+                nol[i]=CheckZero(i);
+            }
+
+            // sorting banyak nol
+            for(int i=0;i<this.row;i++){
+                for(int j=i+1;j<this.row;j++){
+                    if(nol[i]>nol[j]){
+                        int temp1;
+                        temp1=nol[j];
+                        nol[j]=nol[i];
+                        nol[i]=temp1;
+
+                        SwapBaris(i, j);
+                    }
+                }
+            }
+            // menjadikan 1
+            boolean cek=true;
+            for(int j=0;j<this.col;j++){
+                if(this.array[idx-1][j]!=0 && cek){
+                    cek=false;
+                    if(this.array[idx-1][j]!=1){
+                        KaliMatriks(1/this.array[idx-1][j], idx-1);
+                    }
+                }
+            }
+            //mencari index pengurang
+            int acuan=-1;
+            
+            for(int i=0;i<this.col;i++){
+                if(this.array[idx-1][i]!=0.0){
+                    acuan=i;
+                    break;
+                }
+            }
+
+            // kurangi dengan index atasnya
+            if(acuan!=-1){
+                for(int i=idx;i<this.row;i++){
+                    pengurang=this.array[i][acuan]/this.array[idx-1][acuan];
+                    if(pengurang!=0.0){
+                        for(int j=acuan;j<this.col;j++){
+                            this.array[i][j]-=pengurang*this.array[idx-1][j];
+                        }
+                        
+                    }
+                }
+            }
+            
+
+            // menjadikan index awal menjadi 1
+            
+            idx++;            
+        }
+        //menjadikan 1
+        boolean cek2=true;
+        for(int j=0;j<this.col;j++){
+            if(this.array[idx-1][j]!=0 && cek2){
+                cek2=false;
+                if(this.array[idx-1][j]!=1){
+                    KaliMatriks(1/this.array[idx-1][j], idx-1);
+                }
+            }
+        }
+
+        boolean [] sol = new boolean[100];
+        for(int i=0;i<this.row;i++){
+            boolean cek=true;
+            for(int j=0;j<this.col-1;j++){
+                if(this.array[i][j]!=0 && cek){
+                    sol[j]=true;
+                    cek=false;
+                }
+            }
+        }
+        
+        int sumsol;
+        sumsol=0;
+        for(int i=0;i<this.col-1;i++){
+            if(sol[i]){
+                sumsol++;
+            }
+        }
+        if(CheckSolution()){
+            for(int i=this.row-1;i>=0;i--){
+                boolean cek=true;
+                for(int j=0;j<=this.col;j++){
+                    // sudah ketemu index pengurang
+                    if(this.array[i][j]!=0 && cek){
+                        cek=false;
+                        for(int k=i-1;k>=0;k--){
+                            if(this.array[k][j]!=0){
+                                KurangmMatriks(k, i, this.array[k][j]);
+                            }
+                        }
+                    }
+                }
+            }            
+        }
+    }
+    float sum1(int col1){
+        float sum;
+        sum=0;
+        for(int i=0;i<this.row;i++){
+            sum+=this.array[i][col1];
+        }
+        return sum;
+    }
+    float sum2(int col1,int col2){
+        float sum;
+        sum=0;
+        for(int i=0;i<this.row;i++){
+            sum+=this.array[i][col1]*this.array[i][col2];
+        }
+        return sum;
+    }
+    void Regresi_Linear_Berganda(){
+        float[][] arrayl = new float[1000][1000];
+        int a;
+        a=this.row;
+
+        for(int i=0;i<this.col;i++){
+            for(int j=0;j<this.col+1;j++){
+                if(i==0){
+                    if(j==0){
+                        arrayl[i][j]=a;
+                    }
+                    else{
+                        arrayl[i][j]=sum1(j-1);
+                    }
+                }
+                else{
+                    if(j==0){
+                        arrayl[i][j]=sum1(i-1);
+                    }
+                    else{
+                        arrayl[i][j]=sum2(i-1,j-1);
+                    }
+                }
+            }
+        }
+        //merubah matriks
+        this.row=this.col;
+        this.col=this.col+1;
+
+        for(int i=0;i<this.row;i++){
+            for(int j=0;j<this.col;j++){
+                this.array[i][j]=arrayl[i][j];
+            }
+        }
+        Hasil_OBE();
+        
+        Display();        
+        
+        //print solusi
+
+        System.out.printf("Y = ");
+        for(int i=0;i<this.row;i++){
+            if(this.array[i][this.col-1]!=0){
+                if(i==0){
+                    System.out.printf("%.3f ",this.array[i][this.col-1]);
+                }
+                else{
+                    if(this.array[i][this.col-1]>0){
+                        System.out.printf("+ %.3f*X%d ",this.array[i][this.col-1],i);
+                    }
+                    else{
+                        System.out.printf("- %.3f*X%d ",-this.array[i][this.col-1],i);
+                        
+                    }
+                }
+            }
+        }
+    }
 }
 
 public class Matrix {
@@ -697,8 +897,10 @@ public class Matrix {
         matriks m = new matriks();
         matriks mm=new matriks();
         m.IsiMatriks();
-        System.out.println("Gauss \n");
-        m.SPL_Gauss();
+        // System.out.println("Gauss \n");
+        // m.SPL_Gauss();
+        System.out.println("RLB\n");
+        m.Regresi_Linear_Berganda();
         // mm.IsiMatriks();
         // System.out.println("Gauss \n");
         // mm.SPL_Gauss();
