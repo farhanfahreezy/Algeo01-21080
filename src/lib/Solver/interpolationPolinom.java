fab8ca5260c2aa679911b81d26842199a02e5cfd
Fajar Maulana Herawan <fajarherawan@192.168.0.28>
2022-09-26 21:25:28 +0700

    Interpolasi Polinom Final

--- src/lib/Solver/interpolationPolinom.java
+++ src/lib/Solver/interpolationPolinom.java
@@ -1,9 +1,14 @@
+
 package lib.Solver;
 import java.util.Scanner;
 import lib.Matrix.Matrix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.lang.Math;
 
-public class interpolation {
+public class interpolationPolinom {
     Matrix point = new Matrix();
     int N;
     double [] konstanta = new double [N];
@@ -42,7 +47,6 @@ public class interpolation {
          */
         double [] solution = new double[this.N];
         this.point.Hasil_OBE();
-        this.point.Display();
         // Substitusi Mundur
         for (int i = this.N - 1;i>=0;i--){
             double sum = 0;
@@ -78,26 +82,82 @@ public class interpolation {
     }
     public String nilaiTaksir(double x){
         /*
-         Mengeluarkan nilai taksir dari hasil persamaan polinom
+         * Spesifikasi Fungsi : diberikan nilai x, menghasilkan nilai taksiran dari x
          */
         double sum = 0;
         for (int i=0;i<this.N;i++){
             sum = sum + this.konstanta[i]*Math.pow(x,i);
         }
+        System.out.printf("P(%.2f) = %.2f\n",x,sum);
         return String.format("P(%.2f) = %.2f\n",x,sum);
     }
-    public void interpolasiInputFile(){
-
+    public void interpolasiInputFile(String filename){
+        /*
+         * Membuat matriks dari masukan file
+         * I.S. Masukkan nama file
+         * F.S. Terbentuk Matriks
+         */
+            Matrix temp = new Matrix();
+        try{
+            FileReader file = new FileReader("test/"+filename);
+            BufferedReader br = new BufferedReader(file);   
+            String line = br.readLine();
+            int i = 0;
+            while (line != null){
+                String [] data = line.split(" ");
+                for (int j=0;j<data.length;j++){
+                    temp.array[i][j] = Double.parseDouble(data[j]);
+                }
+                line = br.readLine();
+                i = i + 1;
+            }
+            br.close();
+            this.N = i;
+            this.point.col = this.N+1;
+            this.point.row = this.N;
+            for (int j= 0;j<i;j++){
+                for (int k=0;k<i+1;k++){
+                    if (k != i){
+                        this.point.array[j][k] = Math.pow(temp.array[j][0], k);
+                    }else{
+                        this.point.array[j][k] = temp.array[j][1];
+                    }
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println("File tidak ditemukan");
+            System.out.println(e);
+        }
     }
-    public void interpolasioutputFile(){
+    public void interpolasioutputFile(String filename, double x){
+        /*
+         * Menulis hasil interpolasi ke file
+         * I.S. Sembarang
+         * F.S. Terbentuk file interpolasi.txt
+         */
+        try{
+            FileWriter file = new FileWriter("hasil/"+filename);
+            file.write(this.persamaan);
+            file.write("\n");
+            file.write(nilaiTaksir(x));
+            file.close();
+        }
+        catch(Exception e){
+            System.out.println("File tidak ditemukan");
+            System.out.println(e);
+        }
 
     }
     public static void main(String[] args) {
-        interpolation test = new interpolation();
-        test.interpolasiInput();
+        interpolationPolinom test = new interpolationPolinom();
+        test.interpolasiInputFile("1.txt");
         test.persamaanPolinom();
-        test.nilaiTaksir(2);
+        test.interpolasioutputFile("interpolasi.txt", 5);
     }
 }
 
 
+
+
+	
