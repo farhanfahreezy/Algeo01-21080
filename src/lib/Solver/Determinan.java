e55abadcfda3a1b9790eae0cee1ef45fe7fe5718
Mohammad Farhan Fahrezy <91862287+farhanfahrezy@users.noreply.github.com>
2022-09-26 23:58:39 +0700

    Masukin Determinan dan SPL_Balikan

---
+++ src/lib/Solver/Determinan.java
@@ -0,0 +1,88 @@
+package lib.Solver;
+
+import lib.Matrix.Matrix;
+
+public class Determinan {
+    public static double DET_Reduksi_Baris_Kofaktor(Matrix DetMat){
+        // KAMUS LOKAL
+        double det=0;
+        int sum0,largest0,i,j,k,i0;
+        int tanda = -1;
+        // ini untuk sub-matrix
+        int x,y;
+
+        // ALGORITMA
+
+        if(DetMat.row==1){          // 1x1
+            det=DetMat.array[0][0];
+        } else if(DetMat.row==2){   // 2x2
+            det=(DetMat.array[0][0]*DetMat.array[1][1])-(DetMat.array[0][1]*DetMat.array[1][0]);
+        } else{                     // 3x3
+            largest0 = 0;
+            i0 = 0;
+
+            // mencari 0 terbanyak
+            for(i=0;i<DetMat.row;i++){
+                sum0 = 0;
+                for(j=0;j<DetMat.col;j++){
+                    if(DetMat.array[i][j]==0.0){
+                        sum0++;
+                    }
+                }
+                if(sum0>largest0){
+                    largest0 = sum0;
+                    i0 = i;
+                }
+            }
+
+            // Tanda untuk +-nya
+            if (i0%2==0){
+                tanda = 1;
+            }
+
+            for(k=0;k<DetMat.row;k++){
+                if(DetMat.array[i0][k]==0){
+                    tanda*=-1;
+                    continue;
+                }else{
+                    // Bikin matrix baru untuk kofaktornya
+                    Matrix KofMat = new Matrix();
+                    KofMat.row=(DetMat.row)-1;
+                    KofMat.col=(DetMat.col)-1;
+
+                    x=0;
+                    y=0;
+                    for(i=0;i<DetMat.row;i++){
+                        for(j=0;j<DetMat.col;j++){
+                            if(i==i0 || j==k){
+                                continue;
+                            }else{
+                                KofMat.array[x][y]=DetMat.array[i][j];
+                                if(y==KofMat.col-1){
+                                    y=0;
+                                    x++;
+                                }else{
+                                    y++;
+                                }
+                            }
+                        }
+                    }
+                    det+=tanda*DetMat.array[i0][k]*DET_Reduksi_Baris_Kofaktor(KofMat);
+                    tanda*=-1;
+
+
+                }
+            }
+        }
+
+
+
+        return det;
+    }
+    // public static void main(String[] args) {
+    //     Matrix a = new Matrix();
+    //     a.IsiMatriks();
+    //     double determinan = DET_Reduksi_Baris_Kofaktor(a);
+    //     System.out.println("Hasil inverse adalah : " + determinan);
+    // }
+}
---
+++ src/lib/Solver/SPL_Balikan.java
@@ -0,0 +1,50 @@
+package lib.Solver;
+
+import lib.Matrix.Matrix;
+
+public class SPL_Balikan {
+
+    public static void Balikan(Matrix inverse){
+        /*I.S. Matrix inverse terdefinisi dan isSquare()*/
+        /*F.s. Matrix inverse sudah berubah menjadi inverse */
+
+        // Kamus Lokal
+        int i,j;
+
+        // Membuat matrix augmented dari inverse
+        Matrix inverse2 = new Matrix();
+        inverse2.copyMatrix(inverse, inverse2);
+        inverse2.col*=2;
+        for(i=0;i<inverse.row;i++){
+            for(j=inverse.col;j<inverse2.col;j++){
+                if(i==(j-inverse.col)){
+                    inverse2.array[i][j]=1.0;
+                } else{
+                    inverse2.array[i][j]=0.0;
+                }
+            }
+        }
+
+        // Melakukan GaussJordan sehingga matrix indentitas berpindah ke kiri
+        inverse2.SPL_GaussJordan();
+
+        //inverse2.Display();
+
+        // Mengcopy kembali hasil balikan ke Matrix inverse
+        for(i=0;i<inverse.row;i++){
+            for(j=0;j<inverse.col;j++){
+                inverse.array[i][j]=inverse2.array[i][j+inverse.col];
+            }
+        }
+        //inverse.Display();
+
+    }
+
+    // public static void main(String[] args){
+    //     Matrix a = new Matrix();
+    //     a.IsiMatriks();
+    //     Balikan(a);
+    //     System.out.println("Hasil inverse adalah :");
+    //     a.Display();
+    // }
+}
