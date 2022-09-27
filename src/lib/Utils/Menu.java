cbb2316e55fd54d064644e3b101f4f7272d77635
Fajar Herawan <13521080@std.stei.itb.ac.id>
2022-09-27 20:19:52 +0700

    Menu

---
+++ src/lib/Utils/Menu.java
@@ -0,0 +1,27 @@
+package lib.Utils;
+
+public class Menu {
+    public static void startMenu() {
+        int pilihan = 0;    
+        System.out.println("====================================");
+        System.out.println("=                                  =");
+        System.out.println("=          TUBES ALGEO 1           =");
+        System.out.println("=                                  =");
+        System.out.println("====================================");
+        System.out.println("=                                  =");
+        System.out.println("= Menu                             =");
+        System.out.println("= 1. Sistem Persamaan Linier       =");
+        System.out.println("= 2. Determinan                    =");
+        System.out.println("= 3. Matriks Balikan               =");
+        System.out.println("= 4. Interpolasi Polinom           =");
+        System.out.println("= 5. Interpolasi Bicubic           =");
+        System.out.println("= 6. Regresi linear berganda       =");
+        System.out.println("= 7. Keluar                        =");
+        System.out.println("=                                  =");
+        System.out.println("====================================");
+        switch (pilihan){
+            case 1 :
+            
+        }
+    }
+}
--- src/lib/Utils/utils.java
+++
@@ -1,8 +0,0 @@
-package lib.Utils;
-
-public class utils {
-    public static String test() {
-        return "ini buat utils (ngecek input valid atau tidak input file dan output file print menu dan lain lain buat di main";
-    }
-    
-}
