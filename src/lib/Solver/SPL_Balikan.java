85c2ad9c774f4c1d674bbb73b967093772222062
Farhan Fahrezy <mohdfarhanfahrezy@gmail.com>
2022-09-28 13:55:41 +0700

    fix kalau tidak punya inverse

--- src/lib/Solver/SPL_Balikan.java
+++ src/lib/Solver/SPL_Balikan.java
@@ -7,7 +7,8 @@ public class SPL_Balikan {
     // Menggunakan GaussJordan
     public static Matrix INV_GaussJordan(Matrix inverse){
         /*I.S. Matrix inverse terdefinisi dan isSquare()*/
-        /*F.s. Matrix inverse sudah berubah menjadi inverse */
+        /*F.S. Matrix inverse sudah berubah menjadi inverse */
+        /*F.S. Jika matrix tidak memiliki balikan, akan diberikan matrix kosong*/
 
         // Kamus Lokal
         int i,j;
@@ -16,30 +17,32 @@ public class SPL_Balikan {
         Matrix inverse2 = new Matrix();
         Matrix inverse3 = new Matrix();
         Matrix.copyMatrix(inverse, inverse2);
-        Matrix.copyMatrix(inverse, inverse3);
+        inverse3.row = inverse.row;
+        inverse3.col = inverse.col;
         inverse2.col*=2;
-        for(i=0;i<inverse.row;i++){
-            for(j=inverse.col;j<inverse2.col;j++){
-                if(i==(j-inverse.col)){
-                    inverse2.array[i][j]=1.0;
-                } else{
-                    inverse2.array[i][j]=0.0;
+
+        if(Determinan.DET_Reduksi_Baris_Kofaktor(inverse)!=0.0){
+            for(i=0;i<inverse.row;i++){
+                for(j=inverse.col;j<inverse2.col;j++){
+                    if(i==(j-inverse.col)){
+                        inverse2.array[i][j]=1.0;
+                    } else{
+                        inverse2.array[i][j]=0.0;
+                    }
+                }
+            }
+
+            // Melakukan GaussJordan sehingga matrix indentitas berpindah ke kiri
+            inverse2.Hasil_OBE();
+
+
+            // Mengcopy kembali hasil balikan ke Matrix inverse3
+            for(i=0;i<inverse.row;i++){
+                for(j=0;j<inverse.col;j++){
+                    inverse3.array[i][j]=inverse2.array[i][j+inverse.col];
                 }
             }
         }
-
-        // Melakukan GaussJordan sehingga matrix indentitas berpindah ke kiri
-        inverse2.Hasil_OBE();
-
-        //inverse2.Display();
-
-        // Mengcopy kembali hasil balikan ke Matrix inverse3
-        for(i=0;i<inverse.row;i++){
-            for(j=0;j<inverse.col;j++){
-                inverse3.array[i][j]=inverse2.array[i][j+inverse.col];
-            }
-        }
-        //inverse.Display();
         return inverse3;
     }
 
@@ -47,6 +50,7 @@ public class SPL_Balikan {
     public static Matrix Balikan_SPL(Matrix inverse){
         /*I.S. Matrix inverse terdefinisi dan isSquare()*/
         /*F.s. Matrix inverse sudah berubah menjadi inverse */
+        /*F.S. Jika matrix tidak memiliki balikan, akan diberikan matrix kosong*/
 
         // KAMUS LOKAL
         double det; 
@@ -58,56 +62,59 @@ public class SPL_Balikan {
 
         // ALGORITMA
         det = Determinan.DET_Reduksi_Baris_Kofaktor(inverse);
-        Matrix.copyMatrix(inverse, esrevni);
+        esrevni.row = inverse.row;
+        esrevni.col = inverse.col;
 
-        if (inverse.row==2){
-            esrevni.array[0][0]=inverse.array[1][1];
-            esrevni.array[0][1]=(-1)*inverse.array[1][0];
-            esrevni.array[1][0]=(-1)*inverse.array[0][1];
-            esrevni.array[1][1]=inverse.array[0][0];
-            for(i=0;i<esrevni.row;i++){
-                esrevni.KaliMatriks(1/det, i);
-            }
-        } else{
-            
-            for(i=0;i<inverse.row;i++){
-                if(i%2==0){
-                    tanda = 1;
-                } else{
-                    tanda = -1;
+        if(Determinan.DET_Reduksi_Baris_Kofaktor(inverse)!=0.0){
+            if (inverse.row==2){
+                esrevni.array[0][0]=inverse.array[1][1];
+                esrevni.array[0][1]=(-1)*inverse.array[1][0];
+                esrevni.array[1][0]=(-1)*inverse.array[0][1];
+                esrevni.array[1][1]=inverse.array[0][0];
+                for(i=0;i<esrevni.row;i++){
+                    esrevni.KaliMatriks(1/det, i);
                 }
-                for(j=0;j<inverse.row;j++){
-                    
-                    // Mengisi matrix kofaktor ke dalam adjoint
-                    Matrix MatKof = new Matrix();
-                    MatKof.row=inverse.row-1;
-                    MatKof.col=inverse.row-1;
+            } else{
+                
+                for(i=0;i<inverse.row;i++){
+                    if(i%2==0){
+                        tanda = 1;
+                    } else{
+                        tanda = -1;
+                    }
+                    for(j=0;j<inverse.row;j++){
+                        
+                        // Mengisi matrix kofaktor ke dalam adjoint
+                        Matrix MatKof = new Matrix();
+                        MatKof.row=inverse.row-1;
+                        MatKof.col=inverse.row-1;
 
-                    m = 0;
-                    n = 0;
-                    for(x=0;x<inverse.row;x++){
-                        for(y=0;y<inverse.col;y++){
-                            if(x!=i && y!=j){
-                                
-                                MatKof.array[m][n]=inverse.array[x][y];
-                                if(n==MatKof.col-1){
-                                    n=0;
-                                    m++;
-                                }else{
-                                    n++;
+                        m = 0;
+                        n = 0;
+                        for(x=0;x<inverse.row;x++){
+                            for(y=0;y<inverse.col;y++){
+                                if(x!=i && y!=j){
+                                    
+                                    MatKof.array[m][n]=inverse.array[x][y];
+                                    if(n==MatKof.col-1){
+                                        n=0;
+                                        m++;
+                                    }else{
+                                        n++;
+                                    }
                                 }
                             }
                         }
+                        esrevni.array[i][j]=tanda*(Determinan.DET_Reduksi_Baris_Kofaktor(MatKof));
+                        tanda*=-1;
                     }
-                    esrevni.array[i][j]=tanda*(Determinan.DET_Reduksi_Baris_Kofaktor(MatKof));
-                    tanda*=-1;
                 }
+                Matrix.transpose(esrevni);
+                for(i=0;i<esrevni.row;i++){
+                    esrevni.KaliMatriks(1/det, i);
+                }
+                
             }
-            Matrix.transpose(esrevni);
-            for(i=0;i<esrevni.row;i++){
-                esrevni.KaliMatriks(1/det, i);
-            }
-            
         }
         return esrevni;
     }
