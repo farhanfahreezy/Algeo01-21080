5266ca44f6918999b7fe15c20f659f04cce196a3
Fajar Herawan <13521080@std.stei.itb.ac.id>
2022-09-28 16:20:29 +0700

    ImageScalling masih menggunakan Nearby Interpolation

--- src/lib/Solver/imageScaling.java
+++ src/lib/Solver/imageScaling.java
@@ -1,7 +1,6 @@
 package lib.Solver;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -37,10 +36,22 @@ public class imageScaling {
          * F.S : Gambar teroutput
          */
         BufferedImage output = new BufferedImage(this.width*2, this.height*2, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D gph = output.createGraphics();
-        gph.drawImage(this.image, 0, 0, this.width, this.height, null);
+        for (int i=0; i<this.width; i++) {
+            for (int j=0; j<this.height; j++) {
+                Color c = new Color(this.image.getRGB(i, j));
+                int red = (int)(c.getRed());
+                int green = (int)(c.getGreen());
+                int blue = (int)(c.getBlue());
+                int alpha = (int)(c.getAlpha());
+                Color newColor = new Color(red, green, blue, alpha);
+                output.setRGB(i*2, j*2, newColor.getRGB());
+                output.setRGB(i*2+1, j*2, newColor.getRGB());
+                output.setRGB(i*2, j*2+1, newColor.getRGB());
+                output.setRGB(i*2+1, j*2+1, newColor.getRGB());
+            }
+        }
         try {
-            File file = new File("test/output.png");
+            File file = new File("hasil/output.png");
             ImageIO.write(output, "png", file);
         } catch (IOException e) {
             System.out.println("File tidak ditemukan");
@@ -49,7 +60,7 @@ public class imageScaling {
     }
     public static void main(String[] args) {
         imageScaling img = new imageScaling();
-        img.inputImage("test.png");
+        img.inputImage("albert.png");
         img.outputImage();
     }
 }
